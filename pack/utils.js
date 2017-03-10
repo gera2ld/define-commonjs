43c734ef8ac2e21101b63963aa6cb925a78cdce5
Gerald <gera2ld@163.com>
2017-03-10 22:14:23 +0800

    fix path separator issue on Windows

--- pack/utils.js
+++ pack/utils.js
@@ -12,15 +12,17 @@ function resolvePath(filepath, base, options) {
   if (base && /^\.\.?(\/|$)/.test(filepath)) {
     filepath = path.join(base, filepath);
   }
-  filepath = path.normalize(filepath).replace(/\\/g, '/');
-  if (path.isAbsolute(filepath)) return filepath;
-  options && options.alias && Object.keys(options.alias).some(key => {
-    if (filepath === key || filepath.startsWith(`${key}/`)) {
-      filepath = options.alias[key] + filepath.slice(key.length);
-      return true;
-    }
-  });
-  return path.resolve(filepath);
+  filepath = path.normalize(filepath);
+  if (!path.isAbsolute(filepath)) {
+    options && options.alias && Object.keys(options.alias).some(key => {
+      if (filepath === key || filepath.startsWith(`${key}/`)) {
+        filepath = options.alias[key] + filepath.slice(key.length);
+        return true;
+      }
+    });
+    filepath = path.resolve(filepath);
+  }
+  return filepath.replace(/\\/g, '/');
 }
 
 exports.promisify = promisify;
